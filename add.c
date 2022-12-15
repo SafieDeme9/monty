291e818614b4caf907f3ebbd980ad73026e0fb4a
SafieDeme9 <safiedeme9@gmail.com>
2022-12-15 18:28:49 -0500

    check 1

---
+++ add.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * _add -  adds the first two nodes of the stack
+ * @stack: stack given by main
+ * @line_cnt: line counter
+ *
+ * Return: void
+ */
+void _add(stack_t **stack, unsigned int line_cnt)
+{
+
+}
---
+++ monty.h
@@ -0,0 +1,119 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+#define INSTRUCTIONS              \
+	{                           \
+		{"push", push},       \
+		    {"pall", pall},   \
+		    {"pint", pint},   \
+		    {"pop", pop},     \
+		    {"swap", swap},   \
+		    {"nop", nop},     \
+		{                     \
+			NULL, NULL      \
+		}                     \
+	}
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+* struct help - argument for the current opcode
+* @data_struct: stack mode, stack (default) and queue
+* @argument: the arguments of the string
+*
+* Description: global structure used to pass data around the functions easily
+*/
+typedef struct help
+{
+	int data_struct;
+	char *argument;
+} help;
+help global;
+
+/* stack utility functions available in linked_list.c */
+stack_t *add_node(stack_t **stack, const int n);
+stack_t *queue_node(stack_t **stack, const int n);
+void free_stack(stack_t *stack);
+size_t print_stack(const stack_t *stack);
+
+void push(stack_t **stack, unsigned int line_cnt);
+void pall(stack_t **stack, unsigned int line_cnt);
+void pint(stack_t **stack, unsigned int line_cnt);
+void swap(stack_t **stack, unsigned int line_cnt);
+void pop(stack_t **stack, unsigned int line_cnt);
+void nop(stack_t **stack, unsigned int line_cnt);
+void isdigit(char *string);
+void isnumber(char *str);
+
+/**
+ * is_digit - checks if a string is a digit
+ * @string: string to check
+ *
+ * Return: 1 if success, 0 if not
+ */
+int is_digit(char *string)
+{
+	if (!string || *string == '\0')
+		return (0);
+	if (*string == '-')
+		string++;
+	while (*string)
+	{
+		if (isdigit(*string) == 0)
+			return (0);
+		string++;
+	}
+	return (1);
+}
+/**
+ * isnumber - checks if a string is a number
+ * @str: provided string
+ *
+ * Return: 1 if the string is a number, else, 0.
+ */
+int isnumber(char *str)
+{
+	int i;
+
+	if (!str)
+		return (0);
+
+	for (i = 0; str[i]; i++)
+		if (i < '0' || i > '9')
+			return (0);
+
+	return (1);
+}
+
+#endif /* MONTY_H */
